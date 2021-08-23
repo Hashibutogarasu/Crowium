38486d4b580926f6e601415f3d4e6bb9133ad9d8
カラス <71928247+Hashibutogarasu@users.noreply.github.com>
2021-08-23 21:25:04 +0900

    Add files via upload

---
+++ Crowium/Crowium/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Crowium
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Enabled = false;
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.CloseButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CloseButton.Location = new System.Drawing.Point(738, 13);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(50, 23);
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.TabStop = false;
+            this.CloseButton.Text = "×";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(12, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(720, 16);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ChangeURL);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CloseButton);
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Crowium/Crowium/Form1.cs
@@ -0,0 +1,131 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Crowium
+{
+
+    public partial class Form1 : Form
+    {
+        //マウスのクリック位置を記憶
+        public CefSettings settings;
+        public int disph;
+        public int dispw;
+        private ToolTip toolTip1;
+        public object urlchange;
+        ChromiumWebBrowser cefBrowser;
+
+
+        public Form1()
+        {
+
+
+            InitializeComponent();
+
+            int disph = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+            //ディスプレイの幅
+            int dispw = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+
+            //結果表示
+
+
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            this.ControlBox = false;
+            this.Text = "";
+
+            this.MaximumSize = Screen.PrimaryScreen.WorkingArea.Size;
+            this.WindowState = FormWindowState.Maximized;
+
+
+
+            CefSettings cefsettings = new CefSettings();
+            Cef.Initialize(cefsettings);
+            cefBrowser = new ChromiumWebBrowser("https://google.com/");
+            cefBrowser.AddressChanged += OnBrowserAddressChanged;
+            cefBrowser.Dock = DockStyle.Bottom;
+            cefBrowser.Height = disph - 90;
+            
+            this.Controls.Add(cefBrowser);            
+
+            int screen_width = Screen.PrimaryScreen.WorkingArea.Width;
+            int screen_height = Screen.PrimaryScreen.WorkingArea.Height;
+            int MenuHeight = screen_height - cefBrowser.Height;
+
+            Debug.Print("ディスプレイの高さ:{0}ピクセル", disph);
+            Debug.Print("ディスプレイの幅:{0}ピクセル", dispw);
+
+            Debug.Print(string.Format("スクリーンの作業領域の幅:{0:d}", screen_width));
+            Debug.Print(string.Format("スクリーンの作業領域の高さ:{0:d}", screen_height));
+
+            Debug.Print("this.Height:" + this.Height);
+            Debug.Print("cefBrowser.Height:" + cefBrowser.Height);
+            Debug.Print("disph:" + disph);
+            
+
+            CloseButton.Height = MenuHeight;
+            CloseButton.Location = new Point(dispw - CloseButton.Width, 0);
+
+            toolTip1.SetToolTip(CloseButton, "閉じる");
+
+            textBox1.Width = dispw - CloseButton.Width - CloseButton.Width;
+
+            this.Text = "Crowium";
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CloseButton.Enabled = true;
+        }
+
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void Minimum_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ChangeURL(object sender, KeyEventArgs e)
+        {
+            
+            switch (e.KeyData)
+            {
+                case Keys.Enter:
+                    try
+                    {
+                        cefBrowser.Load(textBox1.Text);
+                    }
+                    catch(Exception er)
+                    {
+                        Debug.Print(er.ToString());
+                    }
+                    break;
+            }
+        }
+
+        private void OnBrowserAddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            // this.InvokeOnUiThreadIfRequired(() => Text = e.Address);
+            this.Invoke(new Action(() => {
+                textBox1.Text = e.Address;
+                this.Text = "Crowium:" + e.Address;
+            }));
+        }
+
+    }
+}
